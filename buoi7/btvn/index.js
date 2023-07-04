dc08674df56d5e72d78bda79e13805db1552b5bf
LeCongHau1908 <hau08192002@gmail.com>
2023-07-04 19:52:21 +0700

    baitap

--- buoi7/btvn/index.js
+++ buoi7/btvn/index.js
@@ -28,19 +28,19 @@
 
 // document.write("Số ước của"+ n+ "là"+ count);
 // 8.Nhập vào số n. Viết chương trình kiểm tra xem n có phải số nguyên tố không.
-let n = parseInt(prompt('Hãy nhập số n: '));
-let songuyento = true;
-for (let i = 2; i <= Math.sqrt(n); i++) {
-  if (n % i === 0) {
-    songuyento = false;
-    break;
-  }
-}
-if (songuyento && n > 1) {
-  document.write(n, "là số nguyên tố");
-} else {
-  document.write(n, "không là số nguyên tố");
-}
+// let n = parseInt(prompt('Hãy nhập số n: '));
+// let songuyento = true;
+// for (let i = 2; i <= Math.sqrt(n); i++) {
+//   if (n % i === 0) {
+//     songuyento = false;
+//     break;
+//   }
+// }
+// if (songuyento && n > 1) {
+//   document.write(n, "là số nguyên tố");
+// } else {
+//   document.write(n, "không là số nguyên tố");
+// }
 // Nhập vào số n (n >= 2). Hãy in ra màn hình hình vuông có độ dài cạnh = n.
 // let n = parseInt(prompt("Nhập số n")); 
 
---
+++ buoi8/index.js
@@ -0,0 +1,46 @@
+// let numbers = []; 
+
+// for (let i = 100; i >= 1; i--) {
+//   if (i % 2 !== 0) {
+//     numbers.push(i); 
+//   }
+// }
+
+// document.write(numbers); 
+
+
+// let arr = [1, 2, 3, 4];
+
+// document.write(arr[0]);
+// document.write(arr[3]);
+
+
+// let sum = arr[1] + arr[2];
+// document.write(sum); 
+
+
+// let temp = arr[1];
+// arr[1] = arr[3];
+// arr[3] = temp;
+
+// document.write(arr); 
+
+// let mang = [2, 5, 7, 10, 12, 15, 20];
+// console.log("Các số chẵn trong mảng là:");
+// for (let i = 0; i < mang.length; i++) {
+//   if (mang[i] % 2 === 0) {
+//     console.log(mang[i]);
+//   }
+// }
+// let tong = 0;
+// for (let i = 0; i < mang.length; i++) {
+//   tong += mang[i];
+// }
+// console.log("Tổng các phần tử trong mảng là: " + tong);
+// let min = mang[0];
+// for (let i = 1; i < mang.length; i++) {
+//   if (mang[i] < min) {
+//     min = mang[i];
+//   }
+// }
+// console.log("Phần tử nhỏ nhất trong mảng là: " + min);
